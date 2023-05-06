e5f7d10efc189937502589762b3451f9791a14d5
sonnenbergmarc <msonnenberg@live.com>
2023-05-05 19:20:04 -0600

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="header__logo"></div>
+      <div className="header__logo">
+
+      </div>
       
     </div>
   );
