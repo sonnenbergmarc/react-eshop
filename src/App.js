118019a2a02f4c5ec18824afa50b1cedd35e45e1
sonnenbergmarc <msonnenberg@live.com>
2023-05-05 20:24:51 -0600

    Added Header.js and Header.css for project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <div className="header__logo">
-
-      </div>
-      
+      <Header />
     </div>
   );
 }
