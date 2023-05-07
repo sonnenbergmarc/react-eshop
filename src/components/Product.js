e5fe7c442f9959d77d33e128971a69ee0afd49ef
sonnenbergmarc <msonnenberg@live.com>
2023-05-06 20:27:08 -0600

    Styled home and product cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "../css/Product.css";
+import acUnitImage from "../img/acunit-720.webp"
 
 function Product() {
     return (
         <div>
             <div className="product">
                 <div className="product__info">
-                    <p>Title</p>
-                    <p className="proudct__price">$30</p>
-                    <div className="product__rating"></div>
+                    <p>SereneLife Portable Electric Air Conditioner Unit-900W 8000 BTU Power Plug-in AC Cold Indoor Room Conditioning System</p>
+                    <p className="product__price">
+                        <small></small>
+                        <strong>$30</strong>
+                    </p>
+                    <div className="product__rating">⭐⭐</div>
                 </div>
 
-                <img src="" alt="" />
+                <img src={acUnitImage} alt="Air conditioning unit with remote." />
+
+                <button>Add to Basket</button>
             </div>
         </div>
     )
