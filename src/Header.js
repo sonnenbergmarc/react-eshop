1e68a07e3e91d5037de6a07b01f71f18a97b9593
sonnenbergmarc <msonnenberg@live.com>
2023-05-06 18:14:17 -0600

    Changed colour palette and added an image

--- src/Header.js
+++ src/Header.js
@@ -26,8 +26,8 @@ function Header() {
                     <span className="nav__itemLineOne">Your</span>
                     <span className="nav__itemLineTwo">Shop</span>
                 </div>
-                <div className="nav__item">
-                    <ShoppingBasketIcon className="nav__itemBasket"/>
+                <div className="nav__itemBasket">
+                    <ShoppingBasketIcon/>
                     <span className="nav__itemLineTwo nav__basketCount">0</span>
                 </div>
             </div>
